979b18b9081b721edacb6fafb20483344fadb2b9
alexanderlamoureux@hotmail.com <alexanderlamoureux@hotmail.com>
2017-05-28 14:43:46 -0700

    problem 4 working in coherence with the rest

--- app.js
+++ app.js
@@ -56,17 +56,27 @@ sumAndMultiply(4, 7, 5);
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumAndMultiply() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testsumAndMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testSumAndMultiply = [2,3,4]; //eslint-disable-line
+var testArray = [2,3,4];
+function sumArray(testArray) {
+  var myArray = 0;
 
-function sumAndMultiply(testSumAndMultiply){ //eslint-disable-line
-
+  for(var i = 0; i < testArray.length; i++) {
+    myArray = sum(testArray[i], myArray)[0];
+    // console.log(myArray);
+    var sumArrayMsg = testArray.toString() + ' was passed in as an array of numbers, and ' + myArray + ' is their sum.';
+  }
+  return [myArray, sumArrayMsg];
 }
+sumArray(testArray);
+
+
+
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
 
